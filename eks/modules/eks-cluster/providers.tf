41664a58b5540ffa461ee18d7fc0b6a8db73afe6
mnforba <nforbamel@gmail.com>
2023-09-23 16:40:43 -0400

    initial commit of project files

---
+++ eks/modules/eks-cluster/providers.tf
@@ -0,0 +1,7 @@
+
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+  alias   = "us-east-1"
+}
+
---
+++ eks/variables.tf
@@ -0,0 +1,29 @@
+# Default Variables
+variable "profile" {
+  type    = string
+  default = "default"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+
+# EKS Cluster Variables
+variable "cluster_name" {
+  type    = string
+  default = "tf-cluster"
+}
+
+variable "rolearn" {
+  description = "Add admin role to the aws-auth configmap"
+}
+
+# ALB Controller Variables
+
+variable "env_name" {
+  type    = string
+  default = "dev"
+}
+
